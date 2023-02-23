3b938c06c12babe634f5ec2c596e2b8974318a32
franciscojrdev <franciscojunior7222@gmail.com>
2023-02-22 23:54:23 -0300

    feat: auth validation

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,11 @@
+
+
+export async function authValidation(req,res,next){
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if(!token) return res.sendStatus(401)
+
+    
+}
